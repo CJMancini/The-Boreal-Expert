d07a9831e5a0911ff8c7ed00b556aff2dde20e16
CJ <juliosilvacesar2321@gmail.com>
2023-08-25 20:37:15 -0300

    Botania/Apotheosis/LootTables

--- kubejs/server_scripts/loot/loot.js
+++ kubejs/server_scripts/loot/loot.js
@@ -3,3 +3,49 @@ onEvent("lootjs", (event) => {
       .addLootTypeModifier(LootType.CHEST)
       .removeLoot(Ingredient.all)
 });
+
+// LootJS.modifiers((event) => {
+//   event
+//       .addLootTypeModifier(LootType.CHEST)
+//           .pool((pool) => {
+//               pool.rolls([1]);
+//           })
+//               .anyStructure(["minecraft:simple_dungeon"], false)
+//                   .addWeightedLoot(50, [
+//                       Item.of("minecraft:nether_star").withChance(50)
+//                   ]);
+// });
+
+
+// onEvent("lootjs", (event) => {
+//   event
+//       .addBlockLootModifier("minecraft:gravel")
+//       .anyStructure(["minecraft:stronghold", "minecraft:village"], false) 
+//       .thenAdd("minecraft:diamond");
+// });
+
+// onEvent("lootjs", (event) => {
+//   event
+//       .addBlockLootModifier("minecraft:gravel")
+//       .anyDimension("minecraft:nether") 
+//       .thenAdd("minecraft:diamond");
+// });
+
+
+// QUANDO EXPLODE
+// onEvent("lootjs", (event) => {
+//   event
+//       .addBlockLootModifier("minecraft:gravel")
+//       .customCondition({
+//           condition: "minecraft:survives_explosion"
+//       })
+//       .thenAdd("minecraft:diamond");
+// });
+
+
+// LOOT TABLES
+// https://minecraft.fandom.com/wiki/Structure
+
+// minecraft:chest_minecart 
+// minecraft:simple_dungeon
+// minecraft:village
