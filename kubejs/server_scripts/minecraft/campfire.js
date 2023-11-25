c9d7dfca8361f928b0a67a34eb773f4ecae68907
CJ <juliosilvacesar2321@gmail.com>
2023-11-25 17:05:46 -0300

    0.0.3.9 #ultimo
    Correção da Campfire

--- kubejs/server_scripts/minecraft/campfire.js
+++ kubejs/server_scripts/minecraft/campfire.js
@@ -1,14 +1,18 @@
 onEvent('recipes', event => {
 
-    event.campfireCooking('1x minecraft:iron_nugget', 'minecraft:raw_iron')
-    event.campfireCooking('minecraft:cobblestone', 'minecraft:stone')
+    event.campfireCooking('minecraft:iron_nugget', 'minecraft:raw_iron')
+    event.campfireCooking('minecraft:stone', 'minecraft:cobblestone')
+
 
 })
 
-onEvent('item.tooltip', tooltip => {
 
-tooltip.addAdvanced('minecraft:bedrock', (item, advanced, text) => {
-    text.add(0, Text.of('Hello')) // Adds text in first line, pushing the items name down a line. If you want the line below the item name, the index must be 1
-  })
 
-})
+
+// onEvent('item.tooltip', tooltip => {
+
+// tooltip.addAdvanced('minecraft:bedrock', (item, advanced, text) => {
+//     text.add(0, Text.of('Hello')) // Adds text in first line, pushing the items name down a line. If you want the line below the item name, the index must be 1
+//   })
+
+// })
